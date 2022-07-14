1e5d1485b8a9bde26110b6af2a8163ddfdb1ab46
unknown <shayan.baig385@gmail.com>
2022-07-14 18:17:48 +0500

    work

---
+++ app.js
@@ -0,0 +1,122 @@
+// var b = prompt("Enter city");
+
+// b= b.toUpperCase();
+// document.write(b);
+
+// var a = prompt("Enter your fav mobile model")
+// b = (a);
+// for (let i = 0; i < b.length; i++) {
+//    console.log(b);}
+//       var  last = b.charAt(b.length - 1);
+//    document.write(last);
+         
+
+// var a =("pakistani")
+// for (let i = 0; i < a.length; i++) {
+// var txt = a.slice(i, i+1);
+//       if (txt == "n"){
+//           console.log(txt);
+//           console.log(i);
+//       }
+//     }
+
+// var a = ("The quick brown fox jumps over the lazy dog");
+// for (let i = 0; i < a.length; i++) {
+//      var txt = a.slice(i, i+3);
+//            if (txt == "The"){
+//                console.log(txt);
+//                console.log(i);
+//                         }
+//          }
+
+
+// var a = ("pakistani")
+// document.write(a[3]);
+
+// var a = ("Hyderabad");
+
+// var previousvalue =a.slice(0, 4) ; 
+//       var mynewvalue ="Islam" ;
+//       var restofstring =a.slice(5 , 9);
+     
+//       replacedText = mynewvalue + restofstring;
+      
+//       console.log(a)
+//       console.log(replacedText);
+
+// var a = ("Ali and Sami are best friends. They play cricket and football together.");
+// b = a.replace(/and/gi, "&");
+
+// console.log(a);
+// console.log(b)
+
+
+// var num = (3.45214)
+// num = Math.round(3.45214);
+// console.log(num)
+
+// num = Math.ceil(3.45214);
+// console.log(num);
+
+// num = Math.floor(3.45214);
+// console.log(num)
+
+// var num = (-2.678)
+//  num = Math.round(-2.678);
+//  console.log(num)
+
+//  num = Math.ceil(-2.678);
+//  console.log(num);
+
+//  num = Math.floor(-2.678);
+//  console.log(num)
+
+
+// alert(Math.floor(Math.random() * 6 ) + 1)
+
+
+// let number = Math.random();
+// if (number < 0.5) {
+//     console.log("HEAD");
+// } else {
+//     console.log("TAIL");
+// }
+
+
+// var user =prompt("given your secret num");
+// var num = (Math.floor(Math.random() * 10)  + 1)
+// console.log(num);
+// if (num == user) {
+//     console.log("congratulation")
+// }
+
+
+// var num = ("472")
+// num = num++
+// console.log(num)
+// console.log(typeof(num))
+
+
+// let num = 35.36;
+// let text = num.toString();
+// console.log(text)
+// console.log(typeof(text))
+// var str= text.replace(/\./g, '');
+// console.log(str); 
+
+// var percentage = 30 / 45 * 100;
+// let round= percentage.toFixed(2);
+// console.log(percentage)
+// console.log(round);
+
+
+
+// const date = new Date();
+// const month = date.toLocaleString('default', { month: 'long' });
+// alert("Current Month:" + month);
+
+// var currentDay = new Date();
+// currentDay.toString;
+// var b = currentDay.toString();
+// var c = b.slice(0,3);
+// alert("Today is " + c);
